11a2617bb578db3e69bf94638d0a7fdd7b3e82da
ryoonoda <onoda.ryo@genetec.co.jp>
2024-04-26 10:07:17 +0900

    解答後に次の質問に映る機能の追加

--- lib/questionsScreen.dart
+++ lib/questionsScreen.dart
@@ -12,9 +12,17 @@ class QuestionsScreen extends StatefulWidget{
 }
 
 class _QuestionsScreenState extends State<QuestionsScreen> {
+  var currentQuestionIndex = 0;
+
+  void answerQuestion() {
+    setState(() {
+      currentQuestionIndex++;
+    });
+  }
+
   @override
   Widget build(context){
-    final currentQuestion = questions[0];
+    final currentQuestion = questions[currentQuestionIndex];
     return SizedBox(
       width: double.infinity,
       child: Container(
@@ -34,7 +42,7 @@ class _QuestionsScreenState extends State<QuestionsScreen> {
               height: 30,
             ),
             ...currentQuestion.getShuffleAnswer().map((item) {
-              return AnswerButton(buttonText: item, onTap: () {});
+              return AnswerButton(buttonText: item, onTap: answerQuestion);
             }),
           ],
         ),
